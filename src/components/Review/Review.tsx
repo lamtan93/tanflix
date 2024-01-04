eb485dc49895dfdbf0faf89d33066cd3d6bc8c41
lamtan <lamtan93@gmail.com>
2024-01-04 16:35:15 +0100

    fix style review video background

--- src/components/Review/Review.tsx
+++ src/components/Review/Review.tsx
@@ -24,26 +24,26 @@ const Review: FC = () => {
             <div className='review__content'>
                 <div className='review__item'>
                     <Actor name='Victor' imgSrc={actor1} isImgSrcLocal/>
-                    <p className='review__paragraph'>J’aime beaucoup l’application Netflix qui propose beaucoup de choix en therme de séries/films/animés..</p>
+                    <p className='review__paragraph'>J’aime beaucoup l’application Tanflix qui propose beaucoup de choix en therme de séries/films/animés..</p>
                 </div>
                 <div className='review__item'>
                     <Actor name='Hugo' imgSrc={actor2} isImgSrcLocal/>
-                    <p className='review__paragraph'>L’application Netflix est une bonne application. On y retrouve des produits variés pour tous les goûts.</p>
+                    <p className='review__paragraph'>L’application Tanflix est une bonne application. On y retrouve des produits variés pour tous les goûts.</p>
                 </div>
                 <div className='review__item'>
-                    <Actor name='David' imgSrc={actor1} isImgSrcLocal/>
+                    <Actor name='David' imgSrc={actor3} isImgSrcLocal/>
                     <p className='review__paragraph'>L'application est rodée, efficace et rapide . 4 étoiles pour la technique.</p>
                 </div>
                 <div className='review__item'>
-                    <Actor name='Corgi' imgSrc={actor3} isImgSrcLocal/>
+                    <Actor name='Corgi' imgSrc={actor2} isImgSrcLocal/>
                     <p className='review__paragraph'>Application au top, l'expérience est fluide, logique et super bien pensée.</p>
                 </div>
                 <div className='review__item'>
-                    <Actor name='Tan' imgSrc={actor2} isImgSrcLocal/>
-                    <p className='review__paragraph'>J'ai un abonnement netflix pour 4 personnes et toute la fimille en profite. Vaut vraiment le coup.</p>
+                    <Actor name='Tan' imgSrc={actor3} isImgSrcLocal/>
+                    <p className='review__paragraph'>J'ai un abonnement Tanflix pour 4 personnes et toute la fimille en profite. Vaut vraiment le coup.</p>
                 </div>
                 <div className='review__item'>
-                    <Actor name='Corgi' imgSrc={actor3} isImgSrcLocal/>
+                    <Actor name='Corgi' imgSrc={actor1} isImgSrcLocal/>
                     <p className='review__paragraph'>Beaucoup de choix, cette application est rapide et plusieurs langues sont accessibles. En revanche, je ne comprends pas pourquoi certaines séries ne sont pas complètes.</p>
                 </div>
                 
